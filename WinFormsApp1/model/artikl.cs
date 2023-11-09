e67966cd8579e95a9083650b3214e927c79d560b
Nikola Maričević <nikolamaricevicc@gmail.com>
2023-11-09 19:00:01 +0100

    Add project files.

---
+++ WinFormsApp1/model/artikl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+
+{
+    internal class artikl
+    {
+
+        public string ISBN { get; set; }
+        public string naziv { get; set; }
+        public string proizvodac { get; set; }
+        public string kategorija { get; set; }
+        public int broj_dostupnih { get; set; }
+
+
+
+        public int CompareTo(object obj)
+        {
+            int rez = proizvodac.CompareTo(((artikl)obj).proizvodac);
+
+            if (rez == 0) { rez = naziv.CompareTo(((artikl)obj).naziv); }
+            return rez;
+        }
+
+        public override string ToString() => ISBN + " " + naziv + ", " + proizvodac + ", " + kategorija + ", " + broj_dostupnih;
+    }
+}
+
---
+++ WinFormsApp1/model/skladiste.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinFormsApp1;
+
+namespace WinFormsApp1
+{
+    internal class Skladiste
+    {
+        public List<kupac> kupci;
+        public List<artikl> artikli;
+        public List<narudzba> narudzbe;
+    }
+}
